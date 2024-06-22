514766b817a649f52fa9a4e6a5f142aac05e3b3d
Alvin Haruna <harunaalvin69@gmail.com>
2024-06-22 10:30:16 +0100

    added fetch image function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
+import axios from 'axios';
 import React, { useRef } from 'react';
 import { Form } from 'react-bootstrap'
 import './index.css'
 
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGES_PER_PAGE = 20;
+
 const App = () => {
   // set the search input to a useRef hook
   const searchInput = useRef(null);
+
+  // fetch image function
+  const fetchImages = async () => {
+    try {
+      const { data } = await axios.get(
+        `${API_URL}?query=${searchInput.current.value}&page=1&per_pages=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`
+      );
+      console.log('data', data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   // handle search function
   const handleSearch =(event) => {
